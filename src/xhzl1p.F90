f080514a5424f1a4dc793a5abdf0570feef86e09
Vedran Novakovic <venovako@venovako.eu>
2018-11-07 18:02:35 +0100

    Fix a silly indexing bug.

--- src/xhzl1p.F90
+++ src/xhzl1p.F90
@@ -289,10 +289,10 @@ SUBROUTINE XHZL1P(M,N, H,LDH, JVEC, S,LDS, Z,LDZ, JS,JSPAIR, NSWP,CPR,&
                     DO J = 1, L
                        ZTMP1(J) = H(I+(J-1),P)
                        ZTMP2(J) = H(I+(J-1),Q)
-                       DTMP1(J) = DTMP1(J) + JVEC(M) * (REAL(ZTMP1(J))*REAL(ZTMP1(J)) + AIMAG(ZTMP1(J))*AIMAG(ZTMP1(J)))
-                       DTMP2(J) = DTMP2(J) + JVEC(M) * (REAL(ZTMP2(J))*REAL(ZTMP2(J)) + AIMAG(ZTMP2(J))*AIMAG(ZTMP2(J)))
-                       DTMP3(J) = DTMP3(J) + JVEC(M) * (REAL(ZTMP1(J))*REAL(ZTMP2(J)) + AIMAG(ZTMP1(J))*AIMAG(ZTMP2(J)))
-                       DTMP4(J) = DTMP4(J) + JVEC(M) * (REAL(ZTMP1(J))*AIMAG(ZTMP2(J))- AIMAG(ZTMP1(J))*REAL(ZTMP2(J)))
+                       DTMP1(J) = DTMP1(J) + JVEC(I+(J-1)) * (REAL(ZTMP1(J))*REAL(ZTMP1(J)) + AIMAG(ZTMP1(J))*AIMAG(ZTMP1(J)))
+                       DTMP2(J) = DTMP2(J) + JVEC(I+(J-1)) * (REAL(ZTMP2(J))*REAL(ZTMP2(J)) + AIMAG(ZTMP2(J))*AIMAG(ZTMP2(J)))
+                       DTMP3(J) = DTMP3(J) + JVEC(I+(J-1)) * (REAL(ZTMP1(J))*REAL(ZTMP2(J)) + AIMAG(ZTMP1(J))*AIMAG(ZTMP2(J)))
+                       DTMP4(J) = DTMP4(J) + JVEC(I+(J-1)) * (REAL(ZTMP1(J))*AIMAG(ZTMP2(J))- AIMAG(ZTMP1(J))*REAL(ZTMP2(J)))
                     END DO
                  END DO
 
@@ -492,7 +492,7 @@ SUBROUTINE XHZL1P(M,N, H,LDH, JVEC, S,LDS, Z,LDZ, JS,JSPAIR, NSWP,CPR,&
            !DIR$ VECTOR ALWAYS ALIGNED
            DO L = 1, P
               ZTMP1(L) = H(I+(L-1),J)
-              DTMP1(L) = DTMP1(L) + JVEC(M) * (REAL(ZTMP1(L))*REAL(ZTMP1(L)) + AIMAG(ZTMP1(L))*AIMAG(ZTMP1(L)))
+              DTMP1(L) = DTMP1(L) + JVEC(I+(L-1)) * (REAL(ZTMP1(L))*REAL(ZTMP1(L)) + AIMAG(ZTMP1(L))*AIMAG(ZTMP1(L)))
            END DO
         END DO
         DTOL(1) = SUM(DTMP1)
