b70743a6c22b15e69a43e73901257a08441913b4
Vedran Novakovic <venovako@venovako.eu>
2019-02-23 18:58:47 +0100

    Clean up the makefiles.

--- src/gnu.mk
+++ src/gnu.mk
@@ -3,12 +3,6 @@ ARCH=$(shell uname)
 RM=rm -rfv
 AR=ar
 ARFLAGS=rsv
-FC=gfortran
-ifeq ($(ARCH),Darwin)
-CC=clang
-else # Linux
-CC=gcc
-endif # ?Darwin
 CPUFLAGS=-DUSE_GNU -DUSE_X64
 ifdef KIND_SINGLE
 CPUFLAGS += -DKIND_SINGLE=$(KIND_SINGLE)
@@ -19,12 +13,16 @@ endif # KIND_DOUBLE
 ifdef KIND_FILE
 CPUFLAGS += -DKIND_FILE=$(KIND_FILE)
 endif # KIND_FILE
-FORFLAGS=-cpp $(CPUFLAGS) -fdefault-integer-8 -ffree-line-length-none -fopenmp -fstack-arrays
-C11FLAGS=$(CPUFLAGS) -DFORTRAN_INTEGER_KIND=8
+FORFLAGS=-cpp $(CPUFLAGS) -fdefault-integer-8 -ffree-line-length-none -fopenmp -fstack-arrays #-DHAVE_IMAGINARY
+C11FLAGS=$(CPUFLAGS) -DVN_INTEGER_KIND=8 -std=gnu17
 ifeq ($(ARCH),Darwin)
-C11FLAGS += -std=gnu17 -pthread
+CC=clang
+FC=gfortran
+C11FLAGS += -pthread
 else # Linux
-C11FLAGS += -std=gnu11 -fopenmp
+CC=gcc
+FC=gfortran
+C11FLAGS += -fopenmp
 endif # ?Darwin
 ifdef NDEBUG
 OPTFLAGS=-O$(NDEBUG) -march=native
@@ -32,7 +30,7 @@ DBGFLAGS=-DNDEBUG
 ifeq ($(ARCH),Darwin)
 OPTFFLAGS=$(OPTFLAGS) -Wa,-q -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
 OPTCFLAGS=$(OPTFLAGS) -integrated-as
-DBGFFLAGS=$(DBGFLAGS) -fopt-info-optimized-vec
+DBGFFLAGS=$(DBGFLAGS) -fopt-info-optimized-vec -pedantic -Wall -Wextra -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
 DBGCFLAGS=$(DBGFLAGS)
 else # Linux
 OPTFLAGS += -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
@@ -45,6 +43,7 @@ endif # ?Darwin
 FPUFLAGS=-ffp-contract=fast
 FPUFFLAGS=$(FPUFLAGS)
 FPUCFLAGS=$(FPUFLAGS) -fno-math-errno
+OPTFFLAGS += -DMKL_DIRECT_CALL
 else # DEBUG
 OPTFLAGS=-Og -march=native
 ifeq ($(ARCH),Darwin)
