a3b7943976f62c93e8b8d9e7b8f2a07eaf793405
Vedran Novakovic <venovako@venovako.eu>
2019-11-24 20:49:03 +0100

    Update the compiler flags.

--- src/gnu.mk
+++ src/gnu.mk
@@ -8,7 +8,7 @@ endif # ?NDEBUG
 RM=rm -rfv
 AR=ar
 ARFLAGS=rsv
-CPUFLAGS=-DUSE_GNU -DUSE_X64 -fPIC -fexceptions -fno-omit-frame-pointer -rdynamic
+CPUFLAGS=-DUSE_GNU -DUSE_X64 -fPIC -fexceptions -fno-omit-frame-pointer -fopenmp -rdynamic
 ifdef KIND_SINGLE
 CPUFLAGS += -DKIND_SINGLE=$(KIND_SINGLE)
 endif # KIND_SINGLE
@@ -21,54 +21,53 @@ endif # KIND_FILE
 ifdef PROFILE
 CPUFLAGS += -DVN_PROFILE=$(PROFILE) -fno-inline -finstrument-functions
 endif # PROFILE
-FORFLAGS=-cpp $(CPUFLAGS) -fdefault-integer-8 -ffree-line-length-none -fopenmp -fstack-arrays #-DHAVE_IMAGINARY
+FORFLAGS=-cpp $(CPUFLAGS) -fdefault-integer-8 -ffree-line-length-none -fstack-arrays #-DHAVE_IMAGINARY
 C11FLAGS=$(CPUFLAGS) -std=gnu17
 ifeq ($(ARCH),Darwin)
-CC=clang
+CC=gcc-8
 FC=gfortran-8
-C11FLAGS += -pthread
 else # Linux
 CC=gcc
 FC=gfortran
-C11FLAGS += -fopenmp
 endif # ?Darwin
 ifdef NDEBUG
-OPTFLAGS=-O$(NDEBUG) -march=native
-DBGFLAGS=-DNDEBUG
+OPTFLAGS=-O$(NDEBUG) -march=native -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
+DBGFLAGS=-DNDEBUG -fopt-info-optimized-vec -pedantic -Wall -Wextra
 ifeq ($(ARCH),Darwin)
-OPTFFLAGS=$(OPTFLAGS) -Wa,-q -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
-OPTCFLAGS=$(OPTFLAGS) -integrated-as
-DBGFFLAGS=$(DBGFLAGS) -fopt-info-optimized-vec -pedantic -Wall -Wextra -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
-DBGCFLAGS=$(DBGFLAGS)
-else # Linux
-OPTFLAGS += -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
+OPTFLAGS += -Wa,-q
+endif # Darwin
 OPTFFLAGS=$(OPTFLAGS)
 OPTCFLAGS=$(OPTFLAGS)
-DBGFLAGS += -fopt-info-optimized-vec
-DBGFFLAGS=$(DBGFLAGS) -pedantic -Wall -Wextra -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
+DBGFFLAGS=$(DBGFLAGS) -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
 DBGCFLAGS=$(DBGFLAGS)
-endif # ?Darwin
 FPUFLAGS=-ffp-contract=fast
 FPUFFLAGS=$(FPUFLAGS)
-FPUCFLAGS=$(FPUFLAGS) -fno-math-errno
+FPUCFLAGS=$(FPUFLAGS) #-fno-math-errno
 else # DEBUG
 OPTFLAGS=-O$(DEBUG) -march=native
-DBGFLAGS=-$(DEBUG) -fsanitize=address
+DBGFLAGS=-$(DEBUG) -fsanitize=address -pedantic -Wall -Wextra
 ifeq ($(ARCH),Darwin)
-OPTFFLAGS=$(OPTFLAGS) -Wa,-q
-OPTCFLAGS=$(OPTFLAGS) -integrated-as
+OPTFLAGS += -Wa,-q
 else # Linux
-OPTFFLAGS=$(OPTFLAGS)
-OPTCFLAGS=$(OPTFLAGS)
 DBGFLAGS += -fsanitize=leak
 endif # ?Darwin
-DBGFFLAGS=$(DBGFLAGS) -fcheck=all -finit-local-zero -finit-real=snan -finit-derived -pedantic -Wall -Wextra -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
+OPTFFLAGS=$(OPTFLAGS)
+OPTCFLAGS=$(OPTFLAGS)
+DBGFFLAGS=$(DBGFLAGS) -fcheck=all -finit-local-zero -finit-real=snan -finit-derived -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
 DBGCFLAGS=$(DBGFLAGS) -fsanitize=undefined #-ftrapv
 FPUFLAGS=-ffp-contract=fast
 FPUFFLAGS=$(FPUFLAGS) #-ffpe-trap=invalid,zero,overflow
 FPUCFLAGS=$(FPUFLAGS)
 endif # ?NDEBUG
 LIBFLAGS=-I. -I../../JACSD/vn
-LDFLAGS=-L../../JACSD -lvn$(PROFILE)$(DEBUG) -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
+LDFLAGS=-L../../JACSD -lvn$(PROFILE)$(DEBUG)
+ifndef NDEBUG
+ifeq ($(ARCH),Darwin)
+LDFLAGS += -lubsan
+else # Linux
+$(error debug build currently not possible with RH devtoolset)
+endif # ?Darwin
+endif # DEBUG
+LDFLAGS += -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
 FFLAGS=$(OPTFFLAGS) $(DBGFFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFFLAGS)
 CFLAGS=$(OPTCFLAGS) $(DBGCFLAGS) $(LIBFLAGS) $(C11FLAGS) $(FPUCFLAGS)
